7acde5111bd5d8778924a6c40ef9f94f165c091d
Javier <javier_aml@outlook.com>
2022-01-23 22:57:28 -0600

    Agregado endpoint ConsultarArbitros

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const defaultRoute = require('../controllers/DefaultRoute');
 const consultarEquipos = require('../controllers/ConsultarEquipos');
+const consultarArbitros = require('../controllers/ConsultarArbitros');
 
 router.get('/', defaultRoute.get);
 router.get('/ConsultarEquipos', consultarEquipos.get);
+router.get('/ConsultarArbitros', consultarArbitros.get);
 
 module.exports = router;
