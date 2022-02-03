71ec9e57ea9d96cd5af2be008e56057afe97f722
Javier <javier_aml@outlook.com>
2022-02-03 09:09:45 -0600

    agredado cors en app.js y package.json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./app/routes/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.set('port', port);
+app.use(cors());
 app.use('/', routes);
 
 const main = () => app.listen(app.get('port'), () => console.log(`Server is running on port ${app.get('port')}.`));
